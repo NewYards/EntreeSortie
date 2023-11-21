fbbdb0ebfa85032dfe1a8273514145d4cbc32c91
NewYards <43386297+NewYards@users.noreply.github.com>
2023-11-21 14:58:14 +0100

    Create stdes.h
    + struct _ES_FICHIER (FICHIER)
    + temporaire rename stdout -> stdout_io ligne 19
    + temporaire rename stderr -> stderr_io ligne 20

---
+++ stdes.h
@@ -0,0 +1,35 @@
+#ifndef _STDES_H
+#define _STDES_H
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h> 
+
+typedef struct _ES_FICHIER
+{
+    int fd;
+    char mode;
+    char * buffer;
+    char * cursor;
+    size_t size;
+    char * load_data;
+} FICHIER;
+
+extern FICHIER *stdout_io;
+extern FICHIER *stderr_io;
+
+/* mode: 'L' = lecture, 'E' = écriture */
+FICHIER *ouvrir(const char *nom, char mode);
+int fermer(FICHIER*f);
+int lire(void *p, unsigned int taille, unsigned int nbelem, FICHIER *f);
+int ecrire(const void *p, unsigned int taille, unsigned int nbelem, FICHIER *f);
+int vider(FICHIER *f);
+
+int fecriref (FICHIER *f, const char *format, ...);
+/* directly in stdout */
+int ecriref (const char *format, ...);
+int fliref (FICHIER *f, const char *format, ...);
+
+
+#endif
